c3ce3df7eea4da7184ccb772273ab55f42be7e2b
lizan <1586995842@qq.com>
2020-03-16 19:02:26 +0800

    add index

--- src/main/java/com/lizan/community/controller/hello.java
+++ src/main/java/com/lizan/community/controller/hello.java
@@ -1,5 +1,13 @@
 package com.lizan.community.controller;
 
-public class hello {
 
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class hello {
+    @GetMapping("/")
+    public String index(){
+        return "index";
+    }
 }
